c9e48c388136e0ae1b3900672f8015d8c0394d77
Matthew Young <myoung.space.science@gmail.com>
2026-01-30 16:56:32 -0500

    Update tests/conftest.py
    Add symbols and notes to `greek`

--- tests/conftest.py
+++ tests/conftest.py
@@ -22,59 +22,65 @@ def greek():
     hexidecimal code).
     """
     aslist = [
-        # upper-case standard
-        {'symbol': 'Α', 'hex': r'\x391'},
-        {'symbol': 'Β', 'hex': r'\x392'},
-        {'symbol': 'Γ', 'hex': r'\x393'},
-        {'symbol': 'Δ', 'hex': r'\x394'},
-        {'symbol': 'Ε', 'hex': r'\x395'},
-        {'symbol': 'Ζ', 'hex': r'\x396'},
-        {'symbol': 'Η', 'hex': r'\x397'},
-        {'symbol': 'Θ', 'hex': r'\x398'},
-        {'symbol': 'Ι', 'hex': r'\x399'},
-        {'symbol': 'Κ', 'hex': r'\x39A'},
-        {'symbol': 'Λ', 'hex': r'\x39B'},
-        {'symbol': 'Μ', 'hex': r'\x39C'},
-        {'symbol': 'Ν', 'hex': r'\x39D'},
-        {'symbol': 'Ξ', 'hex': r'\x39E'},
-        {'symbol': 'Ο', 'hex': r'\x39F'},
-        {'symbol': 'Π', 'hex': r'\x3A0'},
-        {'symbol': 'Ρ', 'hex': r'\x3A1'},
-        {'symbol': 'Σ', 'hex': r'\x3A3'},
-        {'symbol': 'Τ', 'hex': r'\x3A4'},
-        {'symbol': 'Υ', 'hex': r'\x3A5'},
-        {'symbol': 'Φ', 'hex': r'\x3A6'},
-        {'symbol': 'Χ', 'hex': r'\x3A7'},
-        {'symbol': 'Ψ', 'hex': r'\x3A8'},
-        {'symbol': 'Ω', 'hex': r'\x3A9'},
-        # lower-case standard
-        {'symbol': 'α', 'hex': r'\x3B1'},
-        {'symbol': 'β', 'hex': r'\x3B2'},
-        {'symbol': 'γ', 'hex': r'\x3B3'},
-        {'symbol': 'δ', 'hex': r'\x3B4'},
-        {'symbol': 'ε', 'hex': r'\x3B5'},
-        {'symbol': 'ζ', 'hex': r'\x3B6'},
-        {'symbol': 'η', 'hex': r'\x3B7'},
-        {'symbol': 'θ', 'hex': r'\x3B8'},
-        {'symbol': 'ι', 'hex': r'\x3B9'},
-        {'symbol': 'κ', 'hex': r'\x3BA'},
-        {'symbol': 'λ', 'hex': r'\x3BB'},
-        {'symbol': 'μ', 'hex': r'\x3BC'},
-        {'symbol': 'ν', 'hex': r'\x3BD'},
-        {'symbol': 'ξ', 'hex': r'\x3BE'},
-        {'symbol': 'ο', 'hex': r'\x3BF'},
-        {'symbol': 'π', 'hex': r'\x3C0'},
-        {'symbol': 'ρ', 'hex': r'\x3C1'},
-        {'symbol': 'σ', 'hex': r'\x3C3'},
-        {'symbol': 'τ', 'hex': r'\x3C4'},
-        {'symbol': 'υ', 'hex': r'\x3C5'},
-        {'symbol': 'φ', 'hex': r'\x3C6'},
-        {'symbol': 'χ', 'hex': r'\x3C7'},
-        {'symbol': 'ψ', 'hex': r'\x3C8'},
-        {'symbol': 'ω', 'hex': r'\x3C9'},
-        # non-standard
+        {'symbol': 'Α', 'hex': r'\x391'}, # Alpha
+        {'symbol': 'Β', 'hex': r'\x392'}, # Beta
+        {'symbol': 'Γ', 'hex': r'\x393'}, # Gamma
+        {'symbol': 'Δ', 'hex': r'\x394'}, # Delta
+        {'symbol': 'Ε', 'hex': r'\x395'}, # Epsilon
+        {'symbol': 'Ζ', 'hex': r'\x396'}, # Zeta
+        {'symbol': 'Η', 'hex': r'\x397'}, # Eta
+        {'symbol': 'Θ', 'hex': r'\x398'}, # Theta
+        {'symbol': 'Ι', 'hex': r'\x399'}, # Iota
+        {'symbol': 'Κ', 'hex': r'\x39A'}, # Kappa
+        {'symbol': 'Λ', 'hex': r'\x39B'}, # Lambda
+        {'symbol': 'Μ', 'hex': r'\x39C'}, # Mu
+        {'symbol': 'Ν', 'hex': r'\x39D'}, # Nu
+        {'symbol': 'Ξ', 'hex': r'\x39E'}, # Xi
+        {'symbol': 'Ο', 'hex': r'\x39F'}, # Omicron
+        {'symbol': 'Π', 'hex': r'\x3A0'}, # Pi
+        {'symbol': 'Ρ', 'hex': r'\x3A1'}, # Rho
+        {'symbol': 'Σ', 'hex': r'\x3A3'}, # Sigma
+        {'symbol': 'Τ', 'hex': r'\x3A4'}, # Tau
+        {'symbol': 'Υ', 'hex': r'\x3A5'}, # Upsilon
+        {'symbol': 'Φ', 'hex': r'\x3A6'}, # Phi
+        {'symbol': 'Χ', 'hex': r'\x3A7'}, # Chi
+        {'symbol': 'Ψ', 'hex': r'\x3A8'}, # Psi
+        {'symbol': 'Ω', 'hex': r'\x3A9'}, # Omega
+        {'symbol': 'Ϛ', 'hex': r'\x3DA'}, # Stigma
+        {'symbol': 'α', 'hex': r'\x3B1'}, # alpha
+        {'symbol': 'β', 'hex': r'\x3B2'}, # beta
+        {'symbol': 'γ', 'hex': r'\x3B3'}, # gamma
+        {'symbol': 'δ', 'hex': r'\x3B4'}, # delta
+        {'symbol': 'ε', 'hex': r'\x3B5'}, # varepsilon
+        {'symbol': 'ζ', 'hex': r'\x3B6'}, # zeta
+        {'symbol': 'η', 'hex': r'\x3B7'}, # eta
+        {'symbol': 'θ', 'hex': r'\x3B8'}, # theta
+        {'symbol': 'ι', 'hex': r'\x3B9'}, # iota
+        {'symbol': 'κ', 'hex': r'\x3BA'}, # kappa
+        {'symbol': 'λ', 'hex': r'\x3BB'}, # lambda
+        {'symbol': 'μ', 'hex': r'\x3BC'}, # mu
+        {'symbol': 'ν', 'hex': r'\x3BD'}, # nu
+        {'symbol': 'ξ', 'hex': r'\x3BE'}, # xi
+        {'symbol': 'ο', 'hex': r'\x3BF'}, # omicron
+        {'symbol': 'π', 'hex': r'\x3C0'}, # pi
+        {'symbol': 'ρ', 'hex': r'\x3C1'}, # rho
         {'symbol': 'ς', 'hex': r'\x3C2'}, # final sigma
-        {'symbol': 'Ϛ', 'hex': r'\x3DA'}, # upper-case stigma
-        {'symbol': 'ϛ', 'hex': r'\x3DB'}, # lower-case stigma
+        {'symbol': 'σ', 'hex': r'\x3C3'}, # sigma
+        {'symbol': 'τ', 'hex': r'\x3C4'}, # tau
+        {'symbol': 'υ', 'hex': r'\x3C5'}, # upsilon
+        {'symbol': 'φ', 'hex': r'\x3C6'}, # varphi
+        {'symbol': 'χ', 'hex': r'\x3C7'}, # chi
+        {'symbol': 'ψ', 'hex': r'\x3C8'}, # psi
+        {'symbol': 'ω', 'hex': r'\x3C9'}, # omega
+        {'symbol': 'ϑ', 'hex': r'\x3D1'}, # vartheta
+        {'symbol': 'ϒ', 'hex': r'\x3D2'}, # (Upsilon variant?)
+        {'symbol': 'ϕ', 'hex': r'\x3D5'}, # phi
+        {'symbol': 'ϖ', 'hex': r'\x3D6'}, # varpi
+        {'symbol': 'ϛ', 'hex': r'\x3DB'}, # stigma
+        {'symbol': 'ϰ', 'hex': r'\x3F0'}, # varkappa
+        {'symbol': 'ϱ', 'hex': r'\x3F1'}, # varrho
+        {'symbol': 'ϵ', 'hex': r'\x3F5'}, # (epsilon variant?)
+        {'symbol': 'ϴ', 'hex': r'\x3F4'}, # (theta variant?)
     ]
     return tuple(aslist)
+
